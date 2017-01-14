d8805b60a6140e5c35e6795e4f817ba149d814aa
akilvein <akilvein@gmail.com>
2017-01-15 00:16:58 +0400

    Create duvalMinRotation.h

---
+++ duvalMinRotation.h
@@ -0,0 +1,29 @@
+#include <string>
+
+string minRotation(string s) {
+    s += s;
+    int n = (int)s.length();
+    int i = 0;
+    int ans = 0;
+
+    while (i < n / 2) {
+        ans = i;
+        int j = i+1;
+        int k = i;
+
+        while (j < n && s[k] <= s[j]) {
+            if (s[k] < s[j]) {
+                k = i;
+            } else {
+                k++;
+            }
+            j++;
+        }
+
+        while (i <= k) {
+            i += (j - k);
+        }
+    }
+
+    return s.substr(ans, n / 2);
+}
