43193d5bfeb82e343bec1beab839dcab7f82026f
akilvein <akilvein@gmail.com>
2017-02-11 21:12:23 +0400

    Update treap.h
    minimum functionality add-remove-index + min tests

--- treap.h
+++ treap.h
@@ -9,133 +9,62 @@
 
 namespace std {
 
+class TreapTester;
+
 template <typename KeyType, typename PriorityType>
-struct Treap {
+class Treap {
+    friend class TreapTester;
 
     struct Node {
-
         KeyType key;
         PriorityType priority;
 
-        Node *pLeft;
-        Node *pRight;
-        Node *pParent;
-
+        Node *left = nullptr;
+        Node *right = nullptr;
         size_t size = 1;
 
-        Node(const KeyType &k, const PriorityType &p, Node *left = nullptr, Node *right = nullptr, Node *parent = nullptr) :
-            key(k),
-            priority(p),
-            pLeft(left),
-            pRight(right),
-            pParent(parent) {
-
-            if (left) {
-                size += left->size;
-                left->pParent = this;
-            }
-
-            if (right) {
-                size += right->size;
-                right->pParent = this;
-            }
+        Node(KeyType k, PriorityType p) : key(k), priority(p) {
         }
 
-        void update(const KeyType &k, const PriorityType &p, Node *left, Node *right, Node *parent) {
-            key = k;
-            priority = p;
-            pLeft = left;
-            pRight = right;
-            pParent = parent;
-            size = 1;
-
-            if (pLeft) {
-                size += pLeft->size;
-                pLeft->pParent = this;
-            }
-
-            if (right) {
-                size += pRight->size;
-                pRight->pParent = this;
-            }
+        static size_t getSize(Node *n) {
+            return n ? n->size : 0;
         }
 
-        void update() {
-            size = 1;
-
-            if (pLeft) {
-                size += pLeft->size;
-                pLeft->pParent = this;
-            }
-
-            if (pRight) {
-                size += pRight->size;
-                pRight->pParent = this;
-            }
+        void recalc() {
+            assert(this);
+            size = 1 + getSize(left) + getSize(right);
         }
 
-        static void update(Node *&tree) {
-            if (tree) {
-                tree->update();
-            }
-        }
-
-        static Node *build(const vector<KeyType> &keys, const vector<PriorityType> &priorities) {
-            assert(keys.size() == priorities.size());
-
-            Node *last = new Node(keys[0], priorities[0]);
-
-            for (size_t i = 1; i < keys.size(); i++) {
-                assert(keys[i - 1] <= keys[i]);
-
-                const KeyType &k = keys[i];
-                const PriorityType &p = priorities[i];
-
-                if (p < last->priority) {
-                    last->pRight = new Node(k, p, nullptr, nullptr, last);
-                    last = last->pRight;
-                } else {
-                    Node *current = last;
-
-                    while (current->pParent && current->priority <= p) {
-                        current = current->pParent;
-                    }
-
-                    if (current->priority <= p) {
-                        last = new Node(k, p, current, nullptr, nullptr);
-                    } else {
-                        last = new Node(k, p, current->pRight, nullptr, current);
-                        current->pRight = last;
-                    }
-
-                }
-            }
-
-            while (last->pParent) {
-                last = last->pParent;
-            }
-
-            last->recursiveCalcSize();
-
-            return last;
-        }
-
-        static void split(Node *&tree, const KeyType &key, Node *&left, Node *&right, bool greater = false) {
+        static void split(Node *tree, KeyType key, Node *&left, Node *&right) {
             if (!tree) {
-                left = right = nullptr;
-            } else if ((key < tree->key) || (greater && key <= tree->key)) {
-                Node *newLeft;
-                split(tree->pLeft, key, left, newLeft, greater);
+                left = nullptr;
+                right = nullptr;
+            } else if (key < tree->key) {
+                split(tree->left, key, left, tree->left);
+                assert(tree);
                 right = tree;
-                right->pLeft = newLeft;
-                update(right);
+                right->recalc();
+                assert(left != right);
             } else {
-                Node *newRight;
-                split(tree->pRight, key, newRight, right, greater);
+                split(tree->right, key, tree->right, right);
+                assert(tree);
                 left = tree;
-                left->pRight = newRight;
-                update(left);
+                left->recalc();
+                assert(left != right);
             }
+        }
+
+        static void insert(Node *&tree, Node *item) {
+            if (!tree) {
+                tree = item;
+            } else if (item->priority > tree->priority) {
+                split(tree, item->key, item->left, item->right);
+                tree = item;
+            } else {
+                insert(item->key < tree->key ? tree->left : tree->right, item);
+            }
+
+            tree->recalc();
         }
 
         static void merge(Node *&tree, Node *left, Node *right) {
@@ -144,232 +73,224 @@ struct Treap {
             } else if (!right) {
                 tree = left;
             } else if (left->priority > right->priority) {
-                merge(left->pRight, left->pRight, right);
+                assert(left->key < right->key);
+                merge(left->right, left->right, right);
                 tree = left;
-                tree->pParent = nullptr;
-                update(tree);
+                assert(tree);
+                tree->recalc();
             } else {
-                merge(right->pLeft, left, right->pLeft);
+                assert(left->key < right->key);
+                merge(right->left, left, right->left);
                 tree = right;
-                tree->pParent = nullptr;
-                update(tree);
+                assert(tree);
+                tree->recalc();
             }
         }
 
-        static Node *remove(Node *&tree, const KeyType &k) {
-            Node *newLeft;
-            Node *newMedLeft;
-            Node *newRight;
-            Node *med;
+        static void remove(Node *&tree, KeyType key) {
+            if (!tree) {
+                return;
+            }
 
-            split(tree, k, newMedLeft, newRight, false); // k elements go newMedLeft
-            split(newMedLeft, k, newLeft, med, true); // k go to med tree
-
-            merge(tree, newLeft, newRight);
-
-            return med;
-        }
-
-        static void insertLeft(Node *&tree, Node *node) {
-            assert(node->size == 1);
-
-            tree->size++;
-            if (tree->pLeft) {
-                insert(tree->pLeft, node);
+            if (tree->key == key) {
+                merge(tree, tree->left, tree->right);
             } else {
-                tree->pLeft = node;
-                node->pParent = tree;
+                remove(key < tree->key ? tree->left : tree->right, key);
+                if (tree) {
+                    tree->recalc();
+                }
             }
         }
 
-        static void insertRight(Node *&tree, Node *node) {
-            assert(node->size == 1);
-
-            tree->size++;
-            if (tree->pRight) {
-                insert(tree->pRight, node);
-            } else {
-                tree->pRight = node;
-                node->pParent = tree;
-            }
-        }
-
-        static void insert(Node *&tree, Node *node) {
-            assert(node->size == 1);
-
-            if (node->priority > tree->priority) {
-                Node *newLeft;
-                Node *newRight;
-                node->pParent = tree->pParent;
-                split(tree, node->key, newLeft, newRight);
-                tree = node;
-                tree->update(node->key, node->priority, newLeft, newRight, node->pParent);
-            } else if (node->key <= tree->key) {
-                insertLeft(tree, node);
-            } else {
-                insertRight(tree, node);
-            }
-        }
-
-
-        Node *getElementByIndex(size_t i) {
+        Node *getByIndex(size_t i) {
             assert(i < size);
 
-            size_t leftSize = pLeft ? pLeft->size : 0;
+            size_t leftSize = getSize(left);
+
+            if (i < leftSize) {
+                return left->getByIndex(i);
+            }
 
             if (i == leftSize) {
                 return this;
             }
 
-            if (i < leftSize) {
-                assert(pLeft);
-                return pLeft->getElementByIndex(i);
-            }
-
-            assert(pRight);
-            return pRight->getElementByIndex(i - leftSize - 1);
+            assert(right);
+            return right->getByIndex(i - 1 - leftSize);
         }
 
-        size_t recursiveCalcSize() {
-            size = 1;
-
-            if (pLeft) {
-                size += pLeft->recursiveCalcSize();
+        void printFlat() {
+            if (left) {
+                left->printFlat();
             }
 
-            if (pRight) {
-                size += pRight->recursiveCalcSize();
-            }
+            cout << "(" << key << ":" << priority << ") ";
 
-            return size;
+            if (right) {
+                right->printFlat();
+            }
         }
-
-        Node *findMin() {
-            Node *current = this;
-
-            while (current->pLeft) {
-                current = current->pLeft;
-            }
-
-            return current;
-        }
-
-        Node *findMax() {
-            Node *current = this;
-
-            while (current->pRight) {
-                current = current->pRight;
-            }
-
-            return current;
-        }
-
-        Node *next() {
-            if (pRight) {
-                return pRight->findMin();
-            }
-
-            Node *current = this;
-            while (current->pParent) {
-                if (current->pParent->pRight != current) {
-                    return current->pParent;
-                }
-
-                current = current->pParent;
-            }
-
-            return nullptr;
-        }
-
 
         void print(int offset = 0) {
-            cout << string(offset * 2, ' ');
-            cout << "(" << key << " - " << priority << ") size: " << size;
-            if (pParent) {
-                cout << "; parent: " << pParent->key << endl;
+            cout << string(offset * 4, ' ');
+            cout << "(" << key << ":" << priority << ") - " << size << endl;
+            if (left) {
+                left->print(offset + 1);
             } else {
-                cout << "; parent: NULL" << endl;
+                cout << string(offset * 4 + 4, ' ') << "null" << endl;
             }
-
-            if (pLeft) {
-                pLeft->print(offset + 1);
+            if (right) {
+                right->print(offset + 1);
             } else {
-                cout << string(offset * 2 + 2, ' ') << "-----" << endl;
-            }
-
-            if (pRight) {
-                pRight->print(offset + 1);
-            } else {
-                cout << string(offset * 2 + 2, ' ') << "-----" << endl;
+                cout << string(offset * 4 + 4, ' ') << "null" << endl;
             }
         }
-
-
-        vector<pair<KeyType, PriorityType> > elements() {
-            vector<pair<KeyType, PriorityType> > result;
-
-            Node *i = findMin();
-            while (i) {
-                result.push_back(make_pair(i->key, i->priority));
-                i = i->next();
-            }
-
-            return result;
-        }
-
     };
 
-    Node *pRoot = nullptr;
+    Node *mpRoot = nullptr;
 
-    Treap() {}
-
-    Treap(const vector<KeyType> &keys, const vector<PriorityType> &priorities) {
-        pRoot = Node::build(keys, priorities);
+    ~Treap() {
+        delete mpRoot;
     }
 
-    void add(const KeyType &k, const PriorityType &p) {
-        Node *node = new Node(k, p);
-
-        if (pRoot) {
-            Node::insert(pRoot, node);
-        } else {
-            pRoot = node;
-        }
+public:
+    void insert(KeyType k, PriorityType p) {
+        Node::insert(mpRoot, new Node(k, p));
     }
 
-    void remove(const KeyType &k) {
-        if (pRoot) {
-            Node *r = Node::remove(pRoot, k);
-            delete(r);
-        }
+    void remove(KeyType key) {
+        Node::remove(mpRoot, key);
     }
 
-    const KeyType &operator[] (size_t i) {
-        assert(pRoot);
-        assert(i < pRoot->size);
-
-        Node *p = pRoot->getElementByIndex(i);
-        return p->key;
+    size_t size() const {
+        return Node::getSize(mpRoot);
     }
 
     bool isEmpty() {
-        return pRoot == nullptr;
+        return Node::getSize(mpRoot) == 0;
     }
 
-    void print() {
-        cout << "============" << endl;
-        if (pRoot) {
-            pRoot->print();
+    KeyType operator[](size_t i) const {
+        assert(i < size());
+        assert(mpRoot);
+        Node *n = mpRoot->getByIndex(i);
+
+        return n->key;
+    }
+};
+
+
+class TreapTester {
+
+    template <typename KeyType, typename PriorityType>
+    static void printFlat(const Treap<KeyType, PriorityType> &t) {
+        if (t.mpRoot) {
+            t.mpRoot->printFlat();
+            cout << endl;
         }
-        cout << "============" << endl;
     }
 
-    vector<pair<KeyType, PriorityType> > elements() {
-        return pRoot ? pRoot->elements() : vector<pair<KeyType, PriorityType> >();
+    template <typename KeyType, typename PriorityType>
+    static void print(const Treap<KeyType, PriorityType> &t) {
+        cout << "================" << endl;
+        if (t.mpRoot) {
+            t.mpRoot->print();
+        }
+        cout << "================" << endl;
+    }
+public:
+    static void t1() {
+        Treap<int, int> t;
+        assert(t.isEmpty());
+        assert(t.size() == 0);
+
+        t.insert(1, 1);
+        assert(t.size() == 1);
+        t.insert(9, 0);
+        assert(t.size() == 2);
+        t.insert(5, 4);
+        assert(t.size() == 3);
+        t.insert(8, 0);
+        assert(t.size() == 4);
+        t.insert(4, 9);
+        assert(t.size() == 5);
+
+        //print(t);
+        //printFlat(t);
+
+        assert(t[0] == 1);
+        assert(t[1] == 4);
+        assert(t[2] == 5);
+        assert(t[3] == 8);
+        assert(t[4] == 9);
+
+        t.remove(0);
+        assert(t.size() == 5);
+
+        t.remove(1);
+        assert(t[0] == 4);
+        assert(t[1] == 5);
+        assert(t[2] == 8);
+        assert(t[3] == 9);
+
+        t.remove(8);
+        assert(t.size() == 3);
+        assert(t[0] == 4);
+        assert(t[1] == 5);
+        assert(t[2] == 9);
+
+        cout << "t1 passed" << endl;
     }
 
+    static double median(const Treap<int, int> &t) {
+        size_t size = t.size();
+        if (size % 2 == 1) {
+            return t[size / 2];
+        } else {
+            double result = 0;
+            result += (double(t[size / 2 - 1]) / 2);
+            result += (double(t[size / 2]) / 2);
+            return result;
+        }
+        assert(false);
+        return 0;
+    }
+
+    static void t2() {
+        srand(time(0));
+
+        Treap<int, int> t;
+
+        t.insert(1, rand());
+        assert(median(t) == 1);
+
+        t.insert(2, rand());
+        assert(median(t) == 1.5);
+
+        t.insert(1, rand());
+        assert(median(t) == 1);
+
+        t.remove(1);
+        assert(median(t) == 1.5);
+
+        t.remove(2);
+        assert(median(t) == 1);
+
+        t.remove(1);
+        assert(t.isEmpty());
+
+        cout << "t2 passed" << endl;
+    }
+
+    static void main() {
+        t1();
+        t2();
+
+        cout << "cool!" << endl;
+    }
 };
 
 } // namespace std
 
 #endif // TREAP_H
+
